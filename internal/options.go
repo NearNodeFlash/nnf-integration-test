f9c2c86aae3da3cebd1cbd62b19d515e9a0121e1
Dean Roehrich <dean.roehrich@hpe.com>
2023-09-11 14:57:12 -0500

    Containers: Added PreRunTimeoutSeconds + Error Exit Strategy
    Pre/PostRunTimeSeconds are now pointers.
    
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

--- internal/options.go
+++ internal/options.go
@@ -8,6 +8,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	"go.openly.dev/pointy"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	corev1 "k8s.io/api/core/v1"
@@ -299,10 +300,10 @@ func (t *T) Prepare(ctx context.Context, k8sClient client.Client) error {
 		if o.containerProfile.options != nil {
 			opt := o.containerProfile.options
 			if opt.PrerunTimeoutSeconds != nil {
-				profile.Data.PreRunTimeoutSeconds = int64(*opt.PrerunTimeoutSeconds)
+				profile.Data.PreRunTimeoutSeconds = pointy.Int64(int64(*opt.PrerunTimeoutSeconds))
 			}
 			if opt.PostrunTimeoutSeconds != nil {
-				profile.Data.PostRunTimeoutSeconds = int64(*opt.PostrunTimeoutSeconds)
+				profile.Data.PostRunTimeoutSeconds = pointy.Int64(int64(*opt.PostrunTimeoutSeconds))
 			}
 			if opt.RetryLimit != nil {
 				profile.Data.RetryLimit = int32(*opt.RetryLimit)
