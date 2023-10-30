93d63ef7804cced83699f2320c48dec4dca91c1e
Blake Devcich <89158881+bdevcich@users.noreply.github.com>
2023-10-30 08:49:34 -0500

    Use git-version-gen for helper image version (#14)
    Signed-off-by: Blake Devcich <blake.devcich@hpe.com>

--- internal/utils.go
+++ internal/utils.go
@@ -17,6 +17,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+const (
+	helperImage = "ghcr.io/nearnodeflash/nnf-integration-test-helper"
+)
+
 func GetSystemConfiguraton(ctx context.Context, k8sClient client.Client) *dwsv1alpha2.SystemConfiguration {
 	// TODO: Move this to a global variable and initialized in the test suite.
 	// Note that putting the GET in Prepare will not work for things like
@@ -35,6 +39,11 @@ func GetSystemConfiguraton(ctx context.Context, k8sClient client.Client) *dwsv1a
 
 func CurrentContext() (string, error) {
 	out, err := exec.Command("kubectl", "config", "current-context").Output()
+	return strings.TrimRight(string(out), "\r\n"), err
+}
+
+func GetVersion() (string, error) {
+	out, err := exec.Command("./git-version-gen").Output()
 	return strings.TrimRight(string(out), "\r\n"), err
 }
 
@@ -72,6 +81,10 @@ func runHelperPod(ctx context.Context, k8sClient client.Client, t *T, name, comm
 	rabbitName := systemConfig.Spec.StorageNodes[0].Name
 	o := t.options
 
+	tag, err := GetVersion()
+	Expect(err).ToNot(HaveOccurred())
+	tag = strings.Replace(tag, "-dirty", "", 1)
+
 	pod := &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      t.workflow.Name + "-" + name,
@@ -81,9 +94,8 @@ func runHelperPod(ctx context.Context, k8sClient client.Client, t *T, name, comm
 			RestartPolicy: corev1.RestartPolicyNever,
 			NodeName:      rabbitName,
 			Containers: []corev1.Container{{
-				Name: name,
-				// TODO: tie this to the version in the makefile
-				Image:           "ghcr.io/nearnodeflash/nnf-integration-test-helper:master",
+				Name:            name,
+				Image:           fmt.Sprintf("%s:%s", helperImage, tag),
 				ImagePullPolicy: corev1.PullAlways,
 				Command: []string{
 					command,
