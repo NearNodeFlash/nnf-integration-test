778e77b3ce8cf3ea3676c7ef7abbfe2e30601990
Blake Devcich <blake.devcich@hpe.com>
2024-02-08 08:59:30 -0600

    Add quick way to update NNF deps
    Signed-off-by: Blake Devcich <blake.devcich@hpe.com>

---
+++ update.sh
@@ -0,0 +1,5 @@
+# update all the nnf dependencies on master
+go get github.com/DataWorkflowServices/dws@master
+go get github.com/NearNodeFlash/lustre-fs-operator@master
+go get github.com/NearNodeFlash/nnf-sos@master
+go get github.com/NearNodeFlash/nnf-dm@master
