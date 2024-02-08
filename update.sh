50c038c8435ae838950896cb12f5df14797090b5
Blake Devcich <blake.devcich@hpe.com>
2024-02-08 09:30:33 -0600

    Add a go mod tidy
    Signed-off-by: Blake Devcich <blake.devcich@hpe.com>

--- update.sh
+++ update.sh
@@ -3,3 +3,4 @@ go get github.com/DataWorkflowServices/dws@master
 go get github.com/NearNodeFlash/lustre-fs-operator@master
 go get github.com/NearNodeFlash/nnf-sos@master
 go get github.com/NearNodeFlash/nnf-dm@master
+go mod tidy
