e38f7476cc5e95d17e0664ea344a901a59e69884
Blake Devcich <blake.devcich@hpe.com>
2024-04-15 08:44:36 -0500

    Fix shellcheck issues
    Signed-off-by: Blake Devcich <blake.devcich@hpe.com>

--- helper_image/scripts/copy-out.sh
+++ helper_image/scripts/copy-out.sh
@@ -21,61 +21,61 @@ COPY_IN=$1
 COPY_OUT=$2
 NUM_COMPUTES=$3
 
-if [[ -z "$COPY_IN" ]]; then
+if [[ -z ${COPY_IN} ]]; then
     usage
     exit 1
-elif [[ -z "$COPY_OUT" ]]; then
+elif [[ -z ${COPY_OUT} ]]; then
     usage
     exit 1
-elif [[ -z "$NUM_COMPUTES" ]]; then
+elif [[ -z ${NUM_COMPUTES} ]]; then
     usage
     exit 1
 fi
 
-if [ ! -e "$COPY_IN" ]; then
-    echo "$COPY_IN does not exist"
+if [[ ! -e ${COPY_IN} ]]; then
+    echo "${COPY_IN} does not exist"
     exit 1
 fi
 
-if [[ $NUM_COMPUTES -gt 0 ]]; then
+if [[ ${NUM_COMPUTES} -gt 0 ]]; then
     echo "Checking for index mount directories"
 
     # verify there are $NUM_COMPUTES directories/files
-    LS_OUTPUT=$(/bin/ls -l $COPY_OUT)
-    if ! echo "$LS_OUTPUT" | wc -l | grep $NUM_COMPUTES; then
-        echo "missing index directories, expected $NUM_COMPUTES:"
-        /bin/ls -l $COPY_OUT
+    LS_OUTPUT=$(/bin/ls -l "${COPY_OUT}")
+    if ! echo "${LS_OUTPUT}" | wc -l | grep "${NUM_COMPUTES}"; then
+        echo "missing index directories, expected ${NUM_COMPUTES}:"
+        /bin/ls -l "${COPY_OUT}"
         exit 1
     fi
 
-    COPY_IN_MD5SUM=$(md5sum "$COPY_IN" | awk '{print $1}')
-    echo "$COPY_IN: $COPY_IN_MD5SUM"
+    COPY_IN_MD5SUM=$(md5sum "${COPY_IN}" | awk '{print $1}')
+    echo "${COPY_IN}: ${COPY_IN_MD5SUM}"
 
     # make sure each checksum is the same
-    md5sum $COPY_OUT | while read -r line; do
-        SUM=$(echo $line | awk '{print $1}')
-        FILE=$(echo $line | awk '{print $2}')
-        echo "$FILE: $SUM"
+    md5sum "${COPY_OUT}" | while read -r line; do
+        SUM=$(echo "${line}" | awk '{print $1}')
+        FILE=$(echo "${line}" | awk '{print $2}')
+        echo "${FILE}: ${SUM}"
 
-        if [ ! "$COPY_IN_MD5SUM" = "$SUM" ]; then
+        if [[ ! ${COPY_IN_MD5SUM} = "${SUM}" ]]; then
             echo "md5sums do not match"
             exit 1
         fi
     done
 else
     echo "Not checking for index mount directories"
-    if [ ! -e "$COPY_OUT" ]; then
-        echo "$COPY_OUT does not exist"
+    if [[ ! -e ${COPY_OUT} ]]; then
+        echo "${COPY_OUT} does not exist"
         exit 1
     fi
 
-    COPY_IN_MD5SUM=$(md5sum "$COPY_IN" | awk '{print $1}')
-    COPY_OUT_MD5SUM=$(md5sum "$COPY_OUT" | awk '{print $1}')
+    COPY_IN_MD5SUM=$(md5sum "${COPY_IN}" | awk '{print $1}')
+    COPY_OUT_MD5SUM=$(md5sum "${COPY_OUT}" | awk '{print $1}')
 
-    echo "$COPY_IN: $COPY_IN_MD5SUM"
-    echo "$COPY_OUT: $COPY_OUT_MD5SUM"
+    echo "${COPY_IN}: ${COPY_IN_MD5SUM}"
+    echo "${COPY_OUT}: ${COPY_OUT_MD5SUM}"
 
-    if [ "$COPY_IN_MD5SUM" = "$COPY_OUT_MD5SUM" ]; then
+    if [[ ${COPY_IN_MD5SUM} = "${COPY_OUT_MD5SUM}" ]]; then
         echo "md5sums match"
         exit 0
     else
