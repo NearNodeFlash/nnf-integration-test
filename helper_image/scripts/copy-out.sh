9fa7aa9afa42759b6191cf562fe30fc6e69c1e7d
Blake Devcich <89158881+bdevcich@users.noreply.github.com>
2024-04-18 12:34:50 -0500

    Update copy-out.sh helper_image script to account for index mount directories (#43)
    - Updated copy-out.sh helper script to take in the number of compute
      nodes to verify index mount directories
    - This also signals if index mount directories are verified or not
      (NUM_COMPUTES > 0)
    - Added t.computes to store the compute nodes used for the test. This is
      used to get the length to give to copy-out.sh for NUM_COMPUTES
    - Adjust the dest path to give to copy-out.sh to include `*/` for index
      mount directories
    
    Signed-off-by: Blake Devcich <blake.devcich@hpe.com>

--- helper_image/scripts/copy-out.sh
+++ helper_image/scripts/copy-out.sh
@@ -1,48 +1,90 @@
 #!/bin/bash
 
+set -e
+
 function usage() {
     echo
     echo "Verify the md5sums of two files match"
     echo "This is used after copy_out directives to ensure the copy_in file matches the copy_out file"
     echo
-    echo "Syntax: copy-out.sh FILE_IN FILE_OUT"
+    echo "Syntax: copy-out.sh FILE_IN FILE_OUT <NUM_COMPUTES>"
+    echo
+    echo "Set <NUM_COMPUTES> to 0 when index mount directories are not expected. When expected, the "
+    echo "path should include an asterik to represent the directories it needs to be escaped (\*)"
     echo
     echo "example:"
-    echo "copy-out.sh /lus/global/testuser/test.in /lus/global/testuser/test.out"
+    echo "copy-out.sh /lus/global/testuser/test.in /lus/global/testuser/test.out 0"
+    echo "copy-out.sh /lus/global/testuser/test.in /lus/global/testuser/\*/test.out 4"
     echo
 }
 
 COPY_IN=$1
 COPY_OUT=$2
+NUM_COMPUTES=$3
 
-if [[ -z "$COPY_IN" ]]; then
+if [[ -z ${COPY_IN} ]]; then
     usage
     exit 1
-elif [[ -z "$COPY_OUT" ]]; then
+elif [[ -z ${COPY_OUT} ]]; then
+    usage
+    exit 1
+elif [[ -z ${NUM_COMPUTES} ]]; then
     usage
     exit 1
 fi
 
-if [ ! -e "$COPY_IN" ]; then
-    echo "$COPY_IN does not exist"
+if [[ ! -e ${COPY_IN} ]]; then
+    echo "${COPY_IN} does not exist"
     exit 1
 fi
 
-if [ ! -e "$COPY_OUT" ]; then
-    echo "$COPY_OUT does not exist"
-    exit 1
-fi
+if [[ ${NUM_COMPUTES} -gt 0 ]]; then
+    echo "Checking for index mount directories"
 
-COPY_IN_MD5SUM=$(md5sum "$COPY_IN" | awk '{print $1}')
-COPY_OUT_MD5SUM=$(md5sum "$COPY_OUT" | awk '{print $1}')
+    # remove any escaping or quotes from the path
+    COPY_OUT=${COPY_OUT//\'/}
+    COPY_OUT=${COPY_OUT//\\/}
 
-echo "$COPY_IN: $COPY_IN_MD5SUM"
-echo "$COPY_OUT: $COPY_OUT_MD5SUM"
+    # verify there are $NUM_COMPUTES directories/files
+    LS_OUTPUT=$(/bin/ls -l ${COPY_OUT})
+    if ! echo "${LS_OUTPUT}" | wc -l | grep "${NUM_COMPUTES}"; then
+        echo "missing index directories, expected ${NUM_COMPUTES}:"
+        /bin/ls -l "${COPY_OUT}"
+        exit 1
+    fi
 
-if [ "$COPY_IN_MD5SUM" = "$COPY_OUT_MD5SUM" ]; then
-    echo "md5sums match"
-    exit 0
+    COPY_IN_MD5SUM=$(md5sum "${COPY_IN}" | awk '{print $1}')
+    echo "${COPY_IN}: ${COPY_IN_MD5SUM}"
+
+    # make sure each checksum is the same
+    md5sum ${COPY_OUT} | while read -r line; do
+        SUM=$(echo "${line}" | awk '{print $1}')
+        FILE=$(echo "${line}" | awk '{print $2}')
+        echo "${FILE}: ${SUM}"
+
+        if [[ ! ${COPY_IN_MD5SUM} = "${SUM}" ]]; then
+            echo "md5sums do not match"
+            exit 1
+        fi
+    done
 else
-    echo "md5sums do not match"
-    exit 1
+    echo "Not checking for index mount directories"
+    if [[ ! -e ${COPY_OUT} ]]; then
+        echo "${COPY_OUT} does not exist"
+        exit 1
+    fi
+
+    COPY_IN_MD5SUM=$(md5sum "${COPY_IN}" | awk '{print $1}')
+    COPY_OUT_MD5SUM=$(md5sum "${COPY_OUT}" | awk '{print $1}')
+
+    echo "${COPY_IN}: ${COPY_IN_MD5SUM}"
+    echo "${COPY_OUT}: ${COPY_OUT_MD5SUM}"
+
+    if [[ ${COPY_IN_MD5SUM} = "${COPY_OUT_MD5SUM}" ]]; then
+        echo "md5sums match"
+        exit 0
+    else
+        echo "md5sums do not match"
+        exit 1
+    fi
 fi
