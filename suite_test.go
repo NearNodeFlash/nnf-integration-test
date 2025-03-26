3a90e49c8d69619d6c295ae60f678e365a8cd952
Blake Devcich <blake.devcich@hpe.com>
2025-03-26 15:06:21 -0500

    CRDBUMPER-vendor-new-api
    Vendor v1alpha7 API from github.com/NearNodeFlash/nnf-sos.
    
    ACTION: If any of the code in this repo was referencing non-local
      APIs, the references to them may have been inadvertently
      modified. Verify that any non-local APIs are being referenced
      by their correct versions.
    
    ACTION: Begin by running "make vet". Repair any issues that it finds.
      Then run "make test" and continue repairing issues until the tests
      pass.
    Signed-off-by: Blake Devcich <blake.devcich@hpe.com>

--- suite_test.go
+++ suite_test.go
@@ -43,7 +43,7 @@ import (
 
 	dwsv1alpha3 "github.com/DataWorkflowServices/dws/api/v1alpha3"
 	lusv1alpha1 "github.com/NearNodeFlash/lustre-fs-operator/api/v1alpha1"
-	nnfv1alpha6 "github.com/NearNodeFlash/nnf-sos/api/v1alpha6"
+	nnfv1alpha7 "github.com/NearNodeFlash/nnf-sos/api/v1alpha7"
 )
 
 var (
@@ -113,7 +113,7 @@ var _ = BeforeSuite(func() {
 	err = lusv1alpha1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
-	err = nnfv1alpha6.AddToScheme(scheme.Scheme)
+	err = nnfv1alpha7.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	By("Creating Client")
